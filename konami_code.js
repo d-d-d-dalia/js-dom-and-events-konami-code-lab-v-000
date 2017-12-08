510a48382e10ac2b3ed2da7695103767115fe64e
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-12-08 16:36:02 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -5,8 +5,9 @@ let index = 0;
 function init() {
 	document.addEventListener('keydown', onKeyDownHandler)
 }
-
+// e stands for event
 function onKeyDownHandler(e) {
+// e.detail and e.which are really referring to the same piece of data but bc browser are different, we cover both kinds it could be.
 	const key = parseInt(e.detail || e.which);
 	 
 	  if (key === code[index]) {
